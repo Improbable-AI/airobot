e449cf6fb3e87d66475253fcbf23cbf563502b4f
taochenshh <chentao904@163.com>
2019-10-01 20:49:48 -0400

    test moveit

--- src/airobot/utils/moveit_util.py
+++ src/airobot/utils/moveit_util.py
@@ -1,8 +1,7 @@
-# modified from pyrobot.util
-import rospy
 import sys
-from geometry_msgs.msg import PoseStamped
+
 import moveit_commander
+from geometry_msgs.msg import PoseStamped
 from moveit_commander import conversions
 
 
@@ -17,180 +16,130 @@ class MoveitScene(object):
         """
         moveit_commander.roscpp_initialize(sys.argv)
         self.scene = moveit_commander.PlanningSceneInterface()
-    def add_static_object(self, obj_name, pos, ori, size, ):
-        pass
 
-    
-    def add_world_object_dp(self, id_name, pose, size, frame='/base'):
-        '''
-        Adds the particular object to the moveit planning scene
+    def add_static_obj(self, obj_name, pos, ori, size=None,
+                       obj_type='box', ref_frame='/base_link',
+                       normal=None):
+        """
+        Add static object to the planning scene
 
-        :param id_name: unique id that object should be labeled with
-        :param pose: pose of the object
-        :param size: size of the object
-        :param frame: frame in which the object pose is passed
-        :type id_name: string
-        :type pose: list of double of length 7 (x,y,z, q_x, q_y, q_z, q_w)
-        :type size: tuple of length 3
-        :type frame: string
-        '''
-        assert type(size) is tuple, 'size should be tuple'
-        assert len(size) == 3, 'size should be of length 3'
+        Args:
+            obj_name (str): object name
+            pos (list): position
+            ori (list): orientation. It can be quaternion ([qx, qy, qz, qw])
+                or euler angles ([roll, pitch, yaw])
+            size (float or list or tuple): size of the object.
+                if the object is a plane, size should be None
+                if the object is a box, size can be a float, which means the edge
+                    length of a cube. size can also be a list or tuple of length 3,
+                    the it specifies the 3 edge lengths of the cuboid
+                if the object is a sphere, size is a float, which means the radius
+            obj_type (str): one of ['sphere', 'box', 'plane']
+            ref_frame (str): reference frame on which the pos and ori are specified
+            normal (list or tuple): only used if the object is a plane. It means the
+                normal direction of the plane
+
+        """
+        if not isinstance(pos, list):
+            raise TypeError('pos should be a list')
+        if not isinstance(ori, list):
+            raise TypeError('ori should be a list')
+        if obj_type not in ['sphere', 'box', 'plane']:
+            raise ValueError('Unsupported object type. Only [sphere],'
+                             ' [box], [plane] are supported for now.')
+
+        pose = pos + ori
         pose = conversions.list_to_pose(pose)
         pose_stamped = PoseStamped()
-        pose_stamped.header.frame_id = frame
+        pose_stamped.header.frame_id = ref_frame
         pose_stamped.pose = pose
-        self.scene.add_box(id_name, pose_stamped, size)
-        rospy.sleep(1.0)
-        self.scene.add_box(id_name, pose_stamped, size)
 
-    def remove_world_object(self, id_name):
-        '''
-        Removes a specified object for the Moveit planning scene
-        :param id_name: unique id that object should be labeled with
-        :type frame: string
-        '''
-        self.scene.remove_world_object(id_name)
-        self.scene.remove_world_object(id_name)
+        if obj_type == 'plane':
+            if size is not None:
+                raise ValueError('No size needed for plane')
+            if normal is None:
+                normal = (0, 0, 1)
+            if len(normal) != 3:
+                raise ValueError('Length of the normal should be 3')
+            if isinstance(normal, list):
+                normal = tuple(normal)
+            self.scene.add_plane(obj_name, pose_stamped, normal)
+        elif obj_type == 'box':
+            if isinstance(size, float):
+                size = (size, size, size)
+            elif isinstance(size, list) or isinstance(size, tuple):
+                if len(size) != 3:
+                    raise ValueError('If size is a list or tuple, its length'
+                                     ' should be 3 for box')
+            if isinstance(size, list):
+                size = tuple(size)
+            self.scene.add_box(obj_name, pose_stamped, size)
+        else:
+            if not isinstance(size, float):
+                raise ValueError('Size should a float number for sphere')
+            self.scene.add_sphere(obj_name, pose_stamped, radius=size)
 
-    def attach_arm_object(self, link_name, id_name, pose, size):
-        '''
-        Attaches the specified object to the robot
-        :param link_name: name of the link to which the bject
-                          should be attached
-        :param id_name: unique id associated with the object
-        :param pose: pose of the object
-        :parma size: size of the object
+    def add_dynamic_obj(self, ref_frame, obj_name, pos, ori, size):
+        """
+        Add object to the ref_frame, the object will move with the ref_frame.
+        Only box is supported for now.
 
-        :type link_name: string
-        :type id_name: string
-        :type pose: list of double of length 7 (x,y,z, q_x, q_y, q_z, q_w)
-        :type size: tuple of length 3
-        '''
-        assert type(size) is tuple, 'size should be tuple'
-        assert len(size) == 3, 'size should be of length 3'
+        Args:
+            ref_frame (str):
+            obj_name (str):
+            pos (list):
+            ori (list):
+            size (float or list or tuple):
+
+        Returns:
+
+        """
+        if not isinstance(pos, list):
+            raise TypeError('pos should be a list')
+        if not isinstance(ori, list):
+            raise TypeError('ori should be a list')
+
+        pose = pos + ori
         pose = conversions.list_to_pose(pose)
         pose_stamped = PoseStamped()
-        pose_stamped.header.frame_id = link_name
+        pose_stamped.header.frame_id = ref_frame
         pose_stamped.pose = pose
-        self.scene.attach_box(link_name, id_name, pose_stamped, size)
-        rospy.sleep(1.0)
-        self.scene.attach_box(link_name, id_name, pose_stamped, size)
 
-    def detach_arm_object(self, link_name, id_name, remove_from_world=True):
-        '''
-        Detaches an object earlier attached to the robot
-        :param link_name: name of the link from which the bject
-                          should be detached
-        :param id_name: unique id associated with the object
-        :param remove_from_world: if set true, deletes the
-                                  object from the scene.
+        if isinstance(size, float):
+            size = (size, size, size)
+        elif isinstance(size, list) or isinstance(size, tuple):
+            if len(size) != 3:
+                raise ValueError('If size is a list or tuple, its length'
+                                 ' should be 3 for box')
+        if isinstance(size, list):
+            size = tuple(size)
+        self.scene.attach_box(ref_frame, obj_name, pose_stamped, size)
 
-        :type link_name: string
-        :type id_name: string
-        :type remove_from_world: bool
-        '''
-        self.scene.remove_attached_object(link_name, id_name)
-        self.scene.remove_attached_object(link_name, id_name)
-        if remove_from_world is True:
-            self.remove_world_object(id_name)
+    def remove_obj(self, obj_name):
+        self.scene.remove_world_object(obj_name)
 
-    def remove_all_objects(self):
-        '''
-        Removes all the objects in the current Moveit planning scene
-        '''
-        ## get add objects
-        dict_obj = self.scene.get_objects()
-        ## get attach object
-        dict_attach_obj = self.scene.get_attached_objects()
-        ## remove add objects
-        for i in dict_obj.keys():
-            self.remove_world_object(i)
-        ## remove attached objects
-        for i in dict_attach_obj.keys():
-            self.detach_arm_object(dict_attach_obj[i].link_name, i)
+    def remove_all_objs(self):
+        objs = self.scene.get_objects()
+        objs_attached = self.scene.get_attached_objects()
+        # remove add objects
+        for key in objs.keys():
+            self.remove_obj(key)
+        # remove attached objects
+        for key in objs_attached.keys():
+            self.unlink_obj(objs_attached[key].link_name, key)
 
-    def add_table(self, pose=None, size=None):
-        '''
-        Adds a table in the planning scene in the base frame.
+    def unlink_obj(self, ref_frame, obj_name=None, delete=True):
+        """
+        Unlink the attached object from ref_frame
 
-        :param pose: pose of the object
-        :parma size: size of the object
+        Args:
+            ref_frame: the parent link at which the object is attached to
+            obj_name: the object name
+            delete: If True, the object will be deleted from the scene.
+                Otherwise, the object will be unlinked from the parent link only,
+                but the object is still there in the scene.
 
-
-        :type pose: list of double of length 7 (x,y,z, q_x, q_y, q_z, q_w)
-        :type size: tuple of length 3
-        '''
-        if pose is not None and size is not None:
-            self.add_world_object('table',
-                                  pose=pose,
-                                  size=size)
-        else:
-            # Default table.
-            print('Creating default table.')
-            self.add_world_object('table',
-                                  pose=[0.8, 0.0, -0.23, 0., 0., 0., 1.],
-                                  size=(1.35, 2.0, 0.1))
-
-    def add_kinect(self, pose=None, size=None):
-        '''
-        Adds a kinect object to the planning scene in the base frame.
-        :param pose: pose of the object
-        :parma size: size of the object
-
-
-        :type pose: list of double of length 7 (x,y,z, q_x, q_y, q_z, q_w)
-        :type size: tuple of length 3
-        '''
-        if pose is not None and size is not None:
-            self.add_world_object('kinect',
-                                  pose=pose,
-                                  size=size)
-        else:
-            # Default kinect.
-            print('Creating default kinect.')
-            self.add_world_object('kinect',
-                                  pose=[0., 0.0, 0.75, 0., 0., 0., 1.],
-                                  size=(0.25, 0.25, 0.3))
-
-    def add_gripper(self, pose=None, size=None):
-        '''
-        Attaches gripper object to 'gripper' link.
-
-        :param pose: pose of the object
-        :parma size: size of the object
-
-
-        :type pose: list of double of length 7 (x,y,z, q_x, q_y, q_z, q_w)
-        :type size: tuple of length 3
-        '''
-        if pose is not None and size is not None:
-            self.attach_arm_object('right_gripper',
-                                   'gripper',
-                                   pose=pose,
-                                   size=size)
-        else:
-            # Default gripper.
-            print('Creating default gripper.')
-            self.attach_arm_object('right_gripper',
-                                   'gripper',
-                                   pose=[0., 0.0, 0.07, 0., 0., 0., 1.],
-                                   size=(0.02, 0.1, 0.07))
-
-    def remove_table(self):
-        '''
-        Removes table object from the planning scene
-        '''
-        self.remove_world_object('table')
-
-    def remove_gripper(self):
-        '''
-        Removes table object from the planning scene
-        '''
-        self.detach_object('gripper')
-        rospy.sleep(0.2)
-        self.detach_object('gripper')
-        rospy.sleep(0.2)
-        self.remove_world_object('gripper')
-        rospy.sleep(0.2)
-        self.remove_world_object('gripper')
+        """
+        self.scene.remove_attached_object(ref_frame, obj_name)
+        if delete:
+            self.remove_obj(obj_name)
