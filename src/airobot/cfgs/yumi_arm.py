015d611e088b33faecfacbed4252858b53310c7b
anthonysimeonov <anthsimeonov@gmail.com>
2019-11-12 11:43:43 -0500

    refactored for single arm and double arm base classes, and added nullspace option to single arm compute_ik. needs debugging

--- src/airobot/cfgs/yumi_arm.py
+++ src/airobot/cfgs/yumi_arm.py
@@ -35,6 +35,8 @@ _C.PYBULLET_RESET_POS = [0, 0, -0.1]
 # reset orientation (euler angles) for the robot in pybullet
 _C.PYBULLET_RESET_ORI = [0, 0, 0]
 
+_C.COMPLIANT_GAINS = [10, 10]
+
 
 def get_yumi_arm_cfg():
     return _C.clone()
