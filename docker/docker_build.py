efe1edbae288bb3619acd4591cdb08f26d88ed23
anthonysimeonov <asimeono@mit.edu>
2019-11-19 11:49:12 -0500

    renamed dockerfile for robot specific, new args and docstrings for docker build, docstrings and cleaned urscript util

--- docker/docker_build.py
+++ docker/docker_build.py
@@ -4,37 +4,63 @@ from __future__ import print_function
 
 import argparse
 import os
-import getpass
 import shutil
 
-if __name__ == '__main__':
 
-    user_name = getpass.getuser()
-    default_image_name = "airobot-dev"
+def execute_build(args):
+    """
+    Create docker build command based on user input arguments
+    and execute the command
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--image', type=str,
-                        help="name for new docker image",
-                        default=default_image_name)
-
-    parser.add_argument('--cache', type=int, default=1,
-                        help="0 if should build without using cache")
-
-    args = parser.parse_args()
+    Args:
+        args (argparse.Namespace): Build arguments
+    """
+    if not os.path.exists(args.docker_file):
+        print('Dockerfile %s not found! Exiting' % args.docker_file)
+        return
 
     # copy requirements file from parent into docker folder
     cwd = os.getcwd()
     shutil.copy(cwd+'/../requirements.txt', cwd)
 
     cmd = 'DOCKER_BUILDKIT=1 docker build '
-    cmd += '-t %s ' % args.image
     cmd += '--ssh default '
     cmd += '--network=host '
     if not args.cache:
         cmd += '--no-cache '
-    cmd += '.'
+    cmd += '-t %s -f %s .' % (args.image, args.docker_file)
 
     print('command = \n\n', cmd)
 
-    os.system(cmd)
+    if not args.dry_run:
+        os.system(cmd)
+
+    # removing copied requirements file from docker/ directory
+    os.remove('requirements.txt')
+
+
+if __name__ == '__main__':
+
+    default_image_name = "airobot-dev"
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--image', type=str,
+                        default=default_image_name,
+                        help='name for new docker image')
+
+    parser.add_argument('-c', '--cache', type=int,
+                        default=1,
+                        help='0 if should build without using cache')
+
+    parser.add_argument('-f', '--docker_file', type=str,
+                        default='ur5e_2f140.dockerfile',
+                        help='which Dockerfile to build from')
+
+    parser.add_argument('-d', '--dry_run', type=int,
+                        default=0,
+                        help='1 if we should only print the build command '
+                             'without executing')
+
+    args = parser.parse_args()
+    execute_build(args)
--- src/airobot/utils/urscript_util.py
+++ src/airobot/utils/urscript_util.py
@@ -11,36 +11,26 @@ built off of urscript.py, part of python-urx library
 
 import airobot as ar
 
-# Controller Settings
-CONTROLLER_PORTS = [0, 1]
-CONTROLLER_VOLTAGE = [
-    0,  # 0-5V
-    2,  # 0-10V
-]
-
-# Tool Settings
-TOOL_PORTS = [2, 3]
-TOOL_VOLTAGE = [
-    0,  # 0-5V
-    1,  # 0-10V
-    2,  # 4-20mA
-]
-
-OUTPUT_DOMAIN_VOLTAGE = [
-    0,  # 4-20mA
-    1,  # 0-10V
-]
-
 
 class URScript(object):
+    """
+    Class for creating urscript programs to send to the
+    UR5 controller
+    """
 
     def __init__(self):
-        # The header is code that is before and outside the myProg() method
-        self.header = ""
+        """
+        Constructor, each urscript has a header and a program
+        that runs on the contoller
+        """
         # The program is code inside the myProg() method
         self.program = ""
 
     def __call__(self):
+        """
+        Create a string formatted properly as a urscript program
+        upon call
+        """
         if self.program == "":
             ar.log_debug("urscript program is empty")
             return ""
@@ -50,25 +40,31 @@ class URScript(object):
 
         # Construct the full script
         script = ""
-        if self.header:
-            script = "{}\n\n".format(self.header)
         script = "{}{}".format(script, myprog)
         return script
 
     def reset(self):
-        self.header = ""
+        """
+        Reset the urscript to empty
+        """
         self.program = ""
 
-    def _add_header_to_program(self, header_line):
-        self.header = "{}\n{}".format(self.header, header_line)
-
     def _add_line_to_program(self, new_line):
+        """
+        Add new line to program inside myProg()
+        
+        Args:
+            new_line (str): Line to add to myProg() urscript program
+        """
         self.program = "{}\n\t{}".format(self.program, new_line)
 
     def constrain_unsigned_char(self, value):
         """
         Ensure that unsigned char values are constrained
         to between 0 and 255.
+
+        Args:
+            value (int): Value to ensure is between 0 and 255
         """
         assert (isinstance(value, int))
         if value < 0:
@@ -78,25 +74,71 @@ class URScript(object):
         return value
 
     def sleep(self, value):
+        """
+        Add a sleep command to urscript program, sleep for a
+        specified amount of time
+        
+        Args:
+            value (float): Amount of time in seconds for program
+                to sleep
+        """
         msg = "sleep({})".format(value)
         self._add_line_to_program(msg)
 
     def socket_open(self, socket_host, socket_port, socket_name):
+        """
+        Add a open socket command to urscript program with specified
+        host, port, and name
+        
+        Args:
+            socket_host (str): Host address
+            socket_port (int): Port to open
+            socket_name (str): Name of the socket to use when interacting
+                with the socket after it is opened
+        """
         msg = "socket_open(\"{}\",{},\"{}\")".format(socket_host,
                                                      socket_port,
                                                      socket_name)
         self._add_line_to_program(msg)
 
     def socket_close(self, socket_name):
+        """
+        Add a close socket command to urscript program.
+        
+        Args:
+            socket_name (str): Name of socket to close (must be same as
+                name of socket that was opened previously)
+        """
         msg = "socket_close(\"{}\")".format(socket_name)
         self._add_line_to_program(msg)
 
     def socket_get_var(self, var, socket_name):
+        """
+        Add a command to the program with communicates over a socket
+        connection to get the value of a variable
+        
+        Args:
+            var (str): Name of the variable to obtain the value of
+            socket_name (str): Which socket connection to use for
+                getting the value (must be same as name of socket that was
+                opened previously)
+        """
         msg = "socket_get_var(\"{}\",\"{}\")".format(var, socket_name)
         self._add_line_to_program(msg)
         self.sync()
 
     def socket_set_var(self, var, value, socket_name):
+        """
+        Add a command to the program with communicates over a socket
+        connection to set the value of a variable
+        
+        Args:
+            var (str): Name of the variable to obtain the value of
+            value (int): Value to set the variable to
+            socket_name (str): Which socket connection to use for
+                getting the value (must be same as name of socket that was
+                opened previously)
+        """
         msg = "socket_set_var(\"{}\",{},\"{}\")".format(
             var,
             value,
@@ -104,21 +146,10 @@ class URScript(object):
         self._add_line_to_program(msg)
         self.sync()
 
-    def socket_read_byte_list(self, nbytes, socket_name):
-        msg = "global var_value = socket_read_byte_list({},\"{}\")".format(
-            nbytes,
-            socket_name)
-        self._add_line_to_program(msg)
-        self.sync()
-
-    def socket_send_string(self, message, socket_name):
-        msg = "socket_send_string(\"{}\",\"{}\")".format(
-            message,
-            socket_name)
-        self._add_line_to_program(msg)
-        self.sync()
-
     def sync(self):
+        """
+        Add a sync command to the myProg() urscript program
+        """
         msg = "sync()"
         self._add_line_to_program(msg)
 
