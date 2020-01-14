74f6b883c56d8d5c5ad8aaad1b5c1d29a79dd1ec
anthonysimeonov <asimeono@mit.edu>
2020-01-14 14:10:02 -0500

    custom_ee_tool example flake8 pass and using pb_client

--- examples/ur5e/sim/custom_ee_tool.py
+++ examples/ur5e/sim/custom_ee_tool.py
@@ -1,17 +1,20 @@
 import time
-
 import os
+
 from airobot import Robot
 from airobot.utils.common import euler2quat
-from airobot.utils.pb_util import load_urdf
-from airobot.utils.pb_util import load_geom
-import numpy as np
+from airobot.utils.pb_util import PB_CLIENT
+import pybullet as p
 
 
 def main():
     """
     This function demonstrates how to load a custom
     end effector to the UR5e robot in pybullet
+
+    NOTE: This implementation using PyBullet's createConstraint
+    function is known to have issues with fixed joints (fixed joints
+    are not actually rigidly fixed)
     """
     robot = Robot('ur5e',
                   pb=True,
@@ -19,39 +22,25 @@ def main():
                   arm_cfg={'render': True, 'self_collision': False})
     robot.arm.go_home()
 
-    ori = euler2quat([0, 0, np.pi / 2])
-    table_id = load_urdf('table/table.urdf',
-                         [1, 0, 0.4],
-                         ori,
-                         scaling=0.9)
-
-    box_id = load_geom('box',
-                       size=0.05,
-                       mass=1,
-                       base_pos=[0.6, 0.12, 1.0],
-                       rgba=[1, 0, 0, 1])
-    box_id2 = load_geom('box',
-                        size=[0.06, 0.02, 0.03],
-                        mass=1,
-                        base_pos=[0.6, -0.12, 1.0],
-                        rgba=[0, 0, 1, 1])
-
     # custom EE tool can either be added by making a new separate URDF
-    
+    # or using the load_geom capbility in pb_util file
+
+    # uncomment below to load an example custom URDF
+    from airobot.utils.pb_util import load_urdf
     root_path = os.path.dirname(os.path.realpath(__file__))
+    urdf_path = '../../../src/airobot/urdfs/custom_ee_tools/stick_ee_tool.urdf'
     stick_id = load_urdf(
-        os.path.join(root_path,
-                     "../../../src/airobot/urdfs/custom_ee_tools/stick_ee_tool.urdf"),
+        os.path.join(root_path, urdf_path),
         [0.3, 0.0, 0.0],
         [0.0, 0.0, 0.0, 1.0],
     )
 
-    # or using the load_geom capability in the pb_util file
-    
+    # uncomment below to load a basic geometry with load_geom
+    # from airobot.utils.pb_util import load_geom
     # stick_id = load_geom('cylinder', size=[0.015, 0.1524], mass=0.01,
     #                      base_pos=[0.3, 0.0, 0.0], rgba=[0, 1, 1, 1])
 
-    cid = robot.arm.p.createConstraint(
+    p.createConstraint(
         robot.arm.robot_id,
         robot.arm.ee_link_id,
         stick_id,
@@ -60,8 +49,8 @@ def main():
         jointAxis=[1, 1, 1],
         parentFramePosition=[0, 0, 0],
         childFramePosition=[0, 0, -0.077],
-        childFrameOrientation=euler2quat([0, 0, 0])
-    )
+        childFrameOrientation=euler2quat([0, 0, 0]),
+        physicsClientId=PB_CLIENT)
 
     time.sleep(5)
 
